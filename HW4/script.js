e551ffca41048dfff493bea62fa53109c4b86ee3
evgeniy <evgenkhitev@gmail.com>
2019-11-22 14:54:34 +0300

    added proptotype for fleet units

--- HW4/script.js
+++ HW4/script.js
@@ -1,28 +1,37 @@
-/** Описание классов кораблей */
-const vsd = {
-    name: "Victory Star Destroyer",
-    speed: 60,
-    crew: 6107
+/** Создаем объекты для классов краблей */
+function Ship(name, className, speed, crew) {
+    this.name = name;
+    this.className = className;
+    this.speed = speed;
+    this.crew = crew;
 }
-const acclamator2 = {
-    name: "Acclamator II Cruiser",
-    speed: 40,
-    crew: 2014
-}
+Ship.prototype.toString = function toString() {
+        return `name: ${this.name}\nclass: ${this.className}\nspeed: ${this.speed}\ncrew: ${this.crew}`;
+    };
+const vsd = new Ship ("Victory Star Destroyer", "Battle Cruiser", 60, 6107);
+const acclamator2 = new Ship ("Acclamator II Cruiser", "Cruiser", 40, 2014);
 
+/** создаем объекты для юнитов флота */
+function FleetUnit(name, ships, fleetUnits) {
+    this.name = name;
+    this.ships = ships;
+    this.fleetUnit = fleetUnits;
+}
+FleetUnit.prototype.toString = function() {
+    let outPutFleet = this.name + "\n" + "------------- \n";
+    for (let currentShip of this.ships) {
+        let lineToPrint = currentShip.class.name + " " + currentShip.count + "\n";
+        outPutFleet = outPutFleet + lineToPrint;
+   }
+    return outPutFleet;
+}
 /**
 * Печатает название соединение и
 * список классов кораблей и их количества в соединении
 */
 function printFleetUnitShipList() {
     let fleetUnit = getFleetUnit();
-    print(fleetUnit.name);
-    print("-------------");
-    for (let i=0; i < fleetUnit.ships.length; i++) {
-        let currentShip = fleetUnit.ships[i];
-        let lineToPrint = currentShip.class.name + " " + currentShip.count;
-        print(lineToPrint);
-    }
+    print(fleetUnit.toString());
 }
 
 /**
@@ -32,11 +41,10 @@ function printFleetUnitShipList() {
 function printFleetUnitMinSpeed() {
     let fleetUnit = getFleetUnit();
     let MinShipSpeed = fleetUnit.ships[0].class.speed;
-    for (let i=0; i < fleetUnit.ships.length; i++) {
-        let currentShip = fleetUnit.ships[i];
-        if (MinShipSpeed > currentShip.class.speed) {
+    for (let currentShip of fleetUnit.ships) {
+       if (MinShipSpeed > currentShip.class.speed) {
             MinShipSpeed = currentShip.class.speed
-            }
+       }
     }
     answer = `Минимальная скорость флота равняется ${MinShipSpeed} НГСС`;
     print(answer);
@@ -52,11 +60,11 @@ function printShipClassInfo() {
     let answer;
     switch(shipUnitChoose) {
             case 'Victory Star Destroyer':
-                answer = `name: ${vsd.name}\nspeed: ${vsd.speed}\ncrew: ${vsd.crew}`;
+                answer = vsd.toString();
                 break;
 
             case 'Acclamator II Cruiser':
-                answer = "name: " + acclamator2.name + "\nspeed: " + acclamator2.speed + "\ncrew: " + acclamator2.crew;
+                answer = acclamator2.toString();
                 break;
         }
         print(answer);
@@ -90,9 +98,6 @@ function getFleetUnit() {
     }
 
     // Соединение флота с именем и массивом кораблей.
-    let fleet = {
-        name: "Линия преследования",
-        ships: ships
-    }
-    return fleet;
+    let pursuitLine = new FleetUnit("Линия преследования", ships, new Array());
+    return pursuitLine;
 }
