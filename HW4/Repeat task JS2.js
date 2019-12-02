a0a3356d4b876cd11b02c07f90fab258948e4688
evgenkh82 <evgenkhitev@gmail.com>
2019-12-02 10:50:07 +0300

    my success in second case of tasks

---
+++ HW4/Repeat task JS2.js
@@ -0,0 +1,79 @@
+/* task 1
+if (true);
+    console.log(1);
+if (false);
+    console.log(1);
+*/
+
+/* task 2
+let m = Math.random();
+let n;
+    if (m > 0.5) {
+        n = "больше";}
+    else {n = "меньше";}
+alert (n);
+*/
+
+/* task 3
+let i = 2;
+while(i < 9) {
+    console.log(i++);
+}
+*/
+
+/* task 4
+let i = 45;
+do {
+    console.log(i++)}
+while (i < 67);
+*/
+
+/* task 5
+let i = 45;
+let j = 10;
+let division;
+while (i < 670) {
+division = i%j;
+    if (division == 0) {
+    console.log(i)}
+    i++;
+}
+*/
+
+/* task 6
+let j = 10;
+let division;
+for (i = 45; i < 67; i++) {
+    console.log(i)
+}
+for (i = 45; i < 670; i++) {
+    division = i%j;
+    if (division == 0) {
+    console.log(i)}
+}
+*/
+
+/* task 7
+let n = 9;
+switch (n) {
+    case 1: console.log ("Один");
+    break;
+    case 2: console.log ("Два");
+    break;
+    case 3: console.log ("Три");
+    break;
+    case 4: console.log ("Четыре");
+    break;
+    case 5: console.log ("Пять");
+    break;
+    case 6: console.log ("Шесть");
+    break;
+    case 7: console.log ("Семь");
+    break;
+    case 8: console.log ("восемь");
+    break;
+    case 9: console.log ("Девять");
+    break;
+    default: console.log ("Нет таких значений");
+}
+*/
