4b1298ad3f6a9d68046ceb022a22cc09316bc272
evgenkh82 <evgenkhitev@gmail.com>
2019-12-02 17:10:46 +0300

    my success in third case of tasks

---
+++ HW4/RT 3.js
@@ -0,0 +1,28 @@
+/* function hello1() {
+    alert( 'Привет, JavaScript' );
+}
+hello1();
+*/
+
+/* function hello2() {
+    let name = prompt('имя', 'Василий');
+    switch(name) {
+        case 'Василий':
+            alert( "Привет, Василий");
+        break;
+        default:
+        alert( "Привет, гость");
+    }
+}
+hello2();
+*/
+
+/* function hello2() {
+    let name = prompt('имя', 'Василий');
+    if (name == 'Василий') {
+        alert( "Привет, Василий");}
+    else {
+        alert( "Привет, гость");}
+    }
+hello2();
+*/
