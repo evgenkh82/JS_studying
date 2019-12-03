1d53d3108ad7941dee18a7b116963d284a5669f2
evgenkh82 <evgenkhitev@gmail.com>
2019-12-03 19:06:08 +0300

    my success in third case of tasks

--- HW4/RT 3.js
+++ HW4/RT 3.js
@@ -1,10 +1,12 @@
-/* function hello1() {
+/* task 1
+function hello1() {
     alert( 'Привет, JavaScript' );
 }
 hello1();
 */
 
-/* function hello2() {
+/* task 2
+function hello2() {
     let name = prompt('имя', 'Василий');
     switch(name) {
         case 'Василий':
@@ -26,3 +28,19 @@ hello2();
     }
 hello2();
 */
+
+/*
+function mu1 (n, m) {
+return n * m;
+}
+let result = mu1(9, 3);
+alert(result);
+mu1();
+*/
+
+function repeat(str, n) {
+return str + n;
+}
+let result = repeat("   ", 3);
+alert(result);
+repeat(str, n);
