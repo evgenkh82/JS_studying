22c0e029f021012ba6b0d0deae1066291494025b
Aleksei Khitev <alexkhitev@gmail.com>
2019-12-04 07:40:15 +0300

    Suggest such a style

--- HW4/Repeat task JS.js
+++ HW4/Repeat task JS.js
@@ -1,64 +1,96 @@
-/* 1)
+/*
+* http://htmllab.ru/zadachi-po-javascript-base/
+* Задание 1
+*/
 let str = "Привет!";
 let num = 123;
 let flag = true;
 let txt = "true";
 
-alert(str);
-alert(num);
-alert(flag);
-alert(txt);
-*/
+console.log("zadachi-po-javascript-base: 1");
+console.log("---------");
+console.log(str);
+console.log(num);
+console.log(flag);
+console.log(txt);
+console.log("==========");
 
-/* 2)
+
+/*
+* http://htmllab.ru/zadachi-po-javascript-base/
+* Задание 2
+*/
 let a1 = 5 + 3;
 let a2 = 5 - 3;
 let a3 = 5 * 3;
 let a4 = 5 / 3;
 
-alert(a1);
-alert(a2);
-alert(a3);
-alert(a4);
-*/
+console.log("zadachi-po-javascript-base: 2");
+console.log("---------");
+console.log("a1 = " + a1);
+console.log("a2 = " + a2);
+console.log("a3 = " + a3);
+console.log("a4 = " + a4);
+console.log("==========");
 
-/* 3)
+/*
+* http://htmllab.ru/zadachi-po-javascript-base/
+* Задание 3
+*/
 let a6 = 5 % 3;
 let a7 = 3 % 5;
+console.log("==========");
 /*почему этот возвращает 3??*/
-/*
 let a8 = 5 + '3';
 let a9 = '5' - 3;
 /*а тут я думал, что все приводится к строке*/
-/*
+
 let a10 = 75 + 'кг';
 
-alert(a6);
-alert(a7);
-alert(a8);
-alert(a9);
-alert(a10);
-*/
+console.log("zadachi-po-javascript-base: 3");
+console.log("---------");
+console.log("a6 = " + a6);
+console.log("a7 = " + a7);
+console.log("a8 = " + a8);
+console.log("a9 = " + a9);
+console.log("a10 = " + a10);
+console.log("==========");
 
-/* 4)
+/*
+* http://htmllab.ru/zadachi-po-javascript-base/
+* Задание 4
+*/
 let height = 23;
 let wight = 10;
 let s = height * wight;
-alert(s);
-*/
+console.log("zadachi-po-javascript-base: 4");
+console.log("---------");
+console.log("s = " + s);
+console.log("==========");
 
-/* 5)
+/*
+* http://htmllab.ru/zadachi-po-javascript-base/
+* Задание 5
+*/
 let heightC = 10;
 let dC = 4;
 let v = 3.14 * Math.pow(dC, 2) * heightC;
-alert(v + " " + "м3");
-*/
+console.log("zadachi-po-javascript-base: 5");
+console.log("---------");
+console.log(v + " " + "м3");
+console.log("==========");
 
-/* 6)
+
+/*
+* http://htmllab.ru/zadachi-po-javascript-base/
+* Задание 6
+*/
 let n = 3;
 let m = 4;
 let k = Math.sqrt(Math.pow(n, 2) + Math.pow(m, 2));
-alert(k);
-*/
+console.log("zadachi-po-javascript-base: 6");
+console.log("---------");
+console.log("k = " + k);
+console.log("==========");
 
-let nFib = {form: 1 to};
+//let nFib = {form: 1 to};
